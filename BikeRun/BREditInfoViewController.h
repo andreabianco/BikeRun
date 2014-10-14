fd0724a4178b9023c94ea95977a8076509e7e051
Andrea <andrea.bianco90@gmail.com>
2014-10-15 00:57:17 +0200

    Add database and DBManager

---
+++ BikeRun/BREditInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  BREditInfoViewController.h
+//  BikeRun
+//
+//  Created by Andrea on 15/10/14.
+//  Copyright (c) 2014 mad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BREditInfoViewController : UIViewController
+
+@end
